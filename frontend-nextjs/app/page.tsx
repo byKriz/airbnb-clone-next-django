c3771a1f669b804caeee16d716735512f9f008ef
byKriz <cristianesquivelh@hotmail.com>
2024-03-27 13:26:42 -0400

    instalando y configurando nextjs

---
+++ frontend-nextjs/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>Airbnb Clone</h1>
+
+      <h2 className="text-airbnb">Prueba de texto</h2>
+    </main>
+  );
+}
