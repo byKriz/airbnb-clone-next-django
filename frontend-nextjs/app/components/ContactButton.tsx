f76578ef24c844b85fdf6550d6f739fc16055f5b
byKriz <cristianesquivelh@hotmail.com>
2024-04-07 17:34:50 -0400

    landlord page static

---
+++ frontend-nextjs/app/components/ContactButton.tsx
@@ -0,0 +1,7 @@
+export function ContactButton() {
+  return (
+    <button className="mt-6 py-4 px-6 bg-airbnb hover:bg-airbnb-dark transition duration-300 text-white rounded-xl">
+      Contact
+    </button>
+  );
+}
---
+++ frontend-nextjs/app/landlords/page.tsx
@@ -0,0 +1,34 @@
+import { ContactButton } from "../components/ContactButton";
+import { PropertyList } from "../components/properties/PropertyList";
+import { ShieldIcon } from "../icons/ShieldIcon";
+
+export default function LandlordDatailPage() {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      <section className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        <aside className="col-span-1 mb-4">
+          <div className="flex flex-col items-center p-6 rounded-xl shadow-xl w-full border border-gray-300 relative">
+            <div className="min-w-[100px] min-h-[100px] w-9 h-9 relative">
+              <img
+                src="/landlord.webp"
+                alt="landlord name"
+                className="w-full h-full rounded-full object-cover mb-2"
+              />
+              <div className="absolute size-8 bg-airbnb text-white flex justify-center items-center rounded-full bottom-0 right-0">
+                <ShieldIcon />
+              </div>
+            </div>
+            <span className="text-2xl font-bold">Landlord</span>
+            <span className=" text-sm">Host</span>
+
+            <ContactButton />
+          </div>
+        </aside>
+
+        <div className="col-span-1 md:col-span-3 pl-0 md:pl-6 ">
+          <PropertyList />
+        </div>
+      </section>
+    </main>
+  );
+}
