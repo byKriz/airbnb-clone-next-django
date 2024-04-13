af17a46c9ac4fa43efdaca2ed8accc443a19c9bf
byKriz <cristianesquivelh@hotmail.com>
2024-04-13 01:58:54 -0400

    agregando el menu desplegable

--- frontend-nextjs/app/components/navbar/UserNav.tsx
+++ frontend-nextjs/app/components/navbar/UserNav.tsx
@@ -1,13 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
 import MenuIcon from "@/app/icons/navBar/MenuIcon";
 import UserIcon from "@/app/icons/navBar/UserIcon";
 
 export function UserNav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="p-2 relative inline-block border rounded-full">
-      <button className="flex items-center">
+      <button className="flex items-center" onClick={handleMenu}>
         <MenuIcon />
         <UserIcon />
       </button>
+
+      {isOpen && (
+        <nav className="absolute w-[220px] top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col">
+          <ul className="flex flex-col [&>li]:px-5 [&>li]:py-4">
+            <li className="hover:bg-gray-100 transition">Log in</li>
+            <li className="hover:bg-gray-100 transition">Sign up</li>
+          </ul>
+        </nav>
+      )}
     </div>
   );
 }
---
+++ frontend-nextjs/app/icons/CloseIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface Props {
+    className?: string;
+}
+
+export default function CloseIcon({className}: Props) {
+  return (
+    <svg
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  );
+}
