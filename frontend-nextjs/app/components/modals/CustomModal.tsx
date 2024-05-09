ca3439bffc76c34f68b9c89326c4b3421123422f
byKriz <cristianesquivelh@hotmail.com>
2024-05-09 19:08:23 -0400

    ajuste menor al custom modal

--- frontend-nextjs/app/components/modals/CustomModal.tsx
+++ frontend-nextjs/app/components/modals/CustomModal.tsx
@@ -38,7 +38,7 @@ export function CustomModal({
     <div className="flex items-center justify-center fixed inset-0 z-50 bg-black/60">
       <div className="relative w-[90%] md:w-[80%] lg:w-[700px] my-6 mx-auto h-auto">
         <div
-          className={`transition duration-600 ease-in-out  ${showModal ? "translate-y-0 opacity-100" : "translate-y-full opacity-10"}`}
+          className={`transition duration-300 ease-in-out  ${showModal ? "translate-y-0 opacity-100" : "translate-y-full opacity-10"}`}
         >
           <div className="w-full h-auto rounded-xl relative flex flex-col bg-white">
             <header className="h-[60px] flex justify-center items-center p-6 rounded-t-md relative border-b">
